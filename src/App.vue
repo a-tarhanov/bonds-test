376e3b4c85c05f89fae8f6ec9110c6af3b78f7cf
Alexander Tarkhanov <starhanov@mail.ru>
2021-06-19 12:26:15 +0300

    initial markup

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,74 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container mt-5">
+    <table class="table table-sm">
+      <thead>
+      <tr>
+        <th colspan="2" class="border-0"></th>
+        <th class="text-center" colspan="2">5 YRS</th>
+        <th class="text-center" colspan="2">10 YRS</th>
+        <th class="text-center" colspan="2">40 YRS</th>
+      </tr>
+      <tr>
+        <th class="fw-normal text-black-50">DATE SENT</th>
+        <th class="fw-normal text-black-50">COMPANY</th>
+        <th class="fw-normal text-black-50 text-center">FIX</th>
+        <th class="fw-normal text-black-50 text-center">FRN</th>
+        <th class="fw-normal text-black-50 text-center">FIX</th>
+        <th class="fw-normal text-black-50 text-center">FRN</th>
+        <th class="fw-normal text-black-50 text-center">FIX</th>
+        <th class="fw-normal text-black-50 text-center">FRN</th>
+      </tr>
+      </thead>
+      <tbody>
+      <tr>
+        <td>30-Dec-20</td>
+        <td class="fw-bold">Openlane</td>
+        <td class="text-center bg-warning">+50bp</td>
+        <td class="text-center"></td>
+        <td class="text-center">+90bp</td>
+        <td class="text-center">+94bp</td>
+        <td class="text-center bg-warning">+130bp</td>
+        <td class="text-center">+128bp</td>
+      </tr>
+      <tr>
+        <td></td>
+        <td>Yield</td>
+        <td class="text-center">0.873%</td>
+        <td class="text-center"></td>
+        <td class="text-center">1.828%</td>
+        <td class="text-center">1.826%</td>
+        <td class="text-center">2.965%</td>
+        <td class="text-center">2.962%</td>
+      </tr>
+      <tr>
+        <td></td>
+        <td>3MLSpread</td>
+        <td class="text-center">+86bp</td>
+        <td class="text-center"></td>
+        <td class="text-center">+88bp</td>
+        <td class="text-center">+86bp</td>
+        <td class="text-center">+152bp</td>
+        <td class="text-center">+150bp  </td>
+      </tr>
+      <tr>
+        <td></td>
+        <td class="fw-bold text-muted">Opentech</td>
+        <td class="text-center"></td>
+        <td class="text-center"></td>
+        <td class="text-center"></td>
+        <td class="text-center"></td>
+        <td class="text-center"></td>
+        <td class="text-center"></td>
+      </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  name: 'App'
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
